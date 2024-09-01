1b992dd94a435c1dd0bec2851f4ccec5afe19c8f
HammyPig <james.bruce.walden@gmail.com>
2024-09-01 05:13:16 +0000

    style: replace single quotes with double quotes in conf.py

--- source/conf.py
+++ source/conf.py
@@ -1,9 +1,9 @@
-extensions = ['myst_nb', 'sphinx_design', 'sphinx_copybutton']
-myst_enable_extensions = ['colon_fence']
+extensions = ["myst_nb", "sphinx_design", "sphinx_copybutton"]
+myst_enable_extensions = ["colon_fence"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
-html_theme = 'pydata_sphinx_theme'
+html_theme = "pydata_sphinx_theme"
 
 html_theme_options = {
     "footer_start": [],
