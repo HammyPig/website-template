269896952538a8cc158926c45e07801c31af32b9
HammyPig <james.bruce.walden@gmail.com>
2024-09-01 05:10:48 +0000

    feat: remove comments from conf.py

--- source/conf.py
+++ source/conf.py
@@ -1,28 +1,12 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'PROJECT'
 copyright = '2024, AUTHOR'
 author = 'AUTHOR'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ['myst_nb', 'sphinx_design', 'sphinx_copybutton']
 myst_enable_extensions = ['colon_fence']
 
 templates_path = ['_templates']
 exclude_patterns = []
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
