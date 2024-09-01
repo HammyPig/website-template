6c2d58b09bd135472ecc45eb4a1f563884f4bffa
HammyPig <james.bruce.walden@gmail.com>
2024-09-01 05:12:02 +0000

    feat: remove unnecessary fields from conf.py

--- source/conf.py
+++ source/conf.py
@@ -1,15 +1,9 @@
-project = 'PROJECT'
-copyright = '2024, AUTHOR'
-author = 'AUTHOR'
-
 extensions = ['myst_nb', 'sphinx_design', 'sphinx_copybutton']
 myst_enable_extensions = ['colon_fence']
 
 templates_path = ['_templates']
-exclude_patterns = []
 
 html_theme = 'pydata_sphinx_theme'
-html_static_path = ['_static']
 
 html_theme_options = {
     "footer_start": [],
