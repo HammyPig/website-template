c56f94c413a1636eea7149702596e29dc6c619e3
HammyPig <james.bruce.walden@gmail.com>
2024-09-01 06:04:52 +0000

    feat: add example link to github icon

--- source/conf.py
+++ source/conf.py
@@ -18,7 +18,7 @@ html_theme_options = {
     "icon_links": [
         {
             "name": "GitHub",
-            "url": "",
+            "url": "https://github.com/HammyPig/website-template",
             "icon": "fa-brands fa-github",
             "type": "fontawesome"
         }
